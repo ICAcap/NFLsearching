bcea9c11f673991914b0a005597f6a0431757e34
ICAcap <58161542+ICAcap@users.noreply.github.com>
2023-11-22 03:59:42 -0500

    update player.py

--- resources/player.py
+++ resources/player.py
@@ -4,8 +4,10 @@ import os
 from dotenv import load_dotenv
 
 # https://pypi.org/project/python-dotenv/
-environment = load_dotenv()
+load_dotenv()
 
+
+# define a Pydantic data model for a player
 class PlayerModel(BaseModel):
     player_id: str
     name: str
@@ -17,7 +19,9 @@ class PlayerModel(BaseModel):
     age: int = None
     college: str = None
 
+# class to interact with the database for player-related operations
 class PlayerResource:
+    # constructor
     def __init__(self):
         db_username = os.getenv("DB_USERNAME")
         db_password = os.getenv("DB_PASSWORD")
@@ -25,9 +29,11 @@ class PlayerResource:
         db_port = str(os.getenv("DB_PORT"))
         db_name = os.getenv("DB_NAME")
 
+        # following based on https://www.datacamp.com/tutorial/sqlalchemy-tutorial-examples
+
         # create a SQLAlchemy engine https://docs.sqlalchemy.org/en/20/core/engines.html#sqlalchemy.create_engine
         db_url = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
-        print(db_url) # debugging purpose
+        # print(db_url) # debugging purpose
         self.engine = db_al.create_engine(db_url)
 
         self.conn = self.engine.connect()
@@ -36,8 +42,14 @@ class PlayerResource:
 
         # table object for player_basic
         self.player_basic = db_al.Table('player_basic', self.metadata, autoload_with=self.engine)
+
+    """
+    function to retrieve player information by player ID from the database
+    @param player_id: the player id
+    @return result: a list of tuple(s)
+    """
     def get_player_by_id(self, player_id):
         query = self.player_basic.select().where(self.player_basic.columns.player_id == player_id)
         exe = self.conn.execute(query)  # executing the query
-        result = exe.fetchall()  # extracting top 5 results
+        result = exe.fetchall()
         return result
