d2432411f51cf826d14d93c6cacad98c9912d004
EC2 Default User <ec2-user@ip-172-31-33-206.us-east-2.compute.internal>
2023-11-26 06:03:30 +0000

    Update main for running on EC2

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ async def delete_player(player_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001) # local machine
-    # uvicorn.run(app, host="0.0.0.0", port=8001) # cloud machine
+    # uvicorn.run(app, host="127.0.0.1", port=8000) # local machine
+    uvicorn.run(app, host="0.0.0.0", port=8000) # cloud machine
 
 
