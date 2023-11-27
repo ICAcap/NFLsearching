713c146adcc8e69e0686ebc1ac96c93474da0aee
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2023-11-26 19:37:33 -0500

    Updated env init player.py

--- resources/player.py
+++ resources/player.py
@@ -30,7 +30,10 @@ class PlayerResource:
         db_host = os.getenv("DB_HOST")
         db_port = str(os.getenv("DB_PORT"))
         db_name = os.getenv("DB_NAME")
-
+        aws_access_key_id= os.getenv("AWS_ACCCESS_KEY_ID")
+        aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY")
+        aws_region= os.getenv("REGION")
+        
         # following based on https://www.datacamp.com/tutorial/sqlalchemy-tutorial-examples
 
         # create a SQLAlchemy engine https://docs.sqlalchemy.org/en/20/core/engines.html#sqlalchemy.create_engine
