ef2c5be56111d3dc911b97791453a57fa3a6a315
Christopher Demirjian <107006766+cjd2186@users.noreply.github.com>
2023-11-26 20:18:32 -0500

    un-did changes, added changes to main.py

--- resources/player.py
+++ resources/player.py
@@ -30,9 +30,6 @@ class PlayerResource:
         db_host = os.getenv("DB_HOST")
         db_port = str(os.getenv("DB_PORT"))
         db_name = os.getenv("DB_NAME")
-        aws_access_key_id= os.getenv("AWS_ACCCESS_KEY_ID")
-        aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY")
-        aws_region= os.getenv("REGION")
         
         # following based on https://www.datacamp.com/tutorial/sqlalchemy-tutorial-examples
 
